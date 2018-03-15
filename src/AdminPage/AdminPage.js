c3e1833bf56f02bc3c8c39d36ac56f64861144b7
Juliana Bacelar <juliana.univasf@gmail.com>
2018-03-15 00:33:18 -0300

    Create admin pages

--- src/AdminPage/AdminPage.js
+++ src/AdminPage/AdminPage.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
-
+import { Route, Switch } from 'react-router-dom';
 import './AdminPage.css'
 import CourseOfferings from './CourseOfferings'
+import AddCourseOfferings from './AddCourseOfferings'
 
-class Admin extends Component {
-  render() {
-    return (
-      <div className='admin-page'>
-        <CourseOfferings />
-      </div>
-    )
-  }
+const Admin = ({match}) => {
+  return (
+    <div className='admin-page'>
+      <Switch>
+        <Route path={match.url + '/ofertas'} component={AddCourseOfferings}/>
+        <Route exact path={match.url} component={CourseOfferings}/>
+      </Switch>
+    </div>
+  )
 }
 
 export default Admin
