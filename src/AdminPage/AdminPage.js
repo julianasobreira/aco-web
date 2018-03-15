c4740b70cae470def2a953b7d150fcf0d2001e37
Juliana Bacelar <juliana.univasf@gmail.com>
2018-03-14 22:34:50 -0300

    Fix style

--- src/AdminPage/AdminPage.js
+++ src/AdminPage/AdminPage.js
@@ -1,44 +1,13 @@
 import React, { Component } from 'react'
-import XLSX from 'xlsx'
 
 import './AdminPage.css'
 import CourseOfferings from './CourseOfferings'
 
 class Admin extends Component {
-  state = {
-    xlsx: [[]]
-  }
-  handleFileUpload = e => {
-    const files = e.target.files
-    if (!files || files.length == 0) return
-    const file = files[0]
-    const fileReader = new FileReader()
-    fileReader.onload = e => {
-      const filename = file.name
-      // pre-process data
-      let binary = ''
-      const bytes = new Uint8Array(e.target.result)
-      for (let i = 0; i < bytes.byteLength; i++) {
-        binary += String.fromCharCode(bytes[i])
-      }
-
-      const workbook = XLSX.read(binary, {type: 'binary'})
-      const sheet = workbook.Sheets[workbook.SheetNames[0]]
-      const xlsx = XLSX.utils.sheet_to_json(sheet, {header: 1, blankrows: false})
-      this.setState({ xlsx })
-    }
-    fileReader.readAsArrayBuffer(file)
-  }
-
   render() {
     return (
       <div className='admin-page'>
-        <h2>Adicionar Oferta</h2>
-        <input 
-          type='file'
-          accept='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel'
-          onChange={this.handleFileUpload} />
-        <CourseOfferings xlsx={this.state.xlsx}/>
+        <CourseOfferings />
       </div>
     )
   }
--- src/AdminPage/CourseOfferings.js
+++ src/AdminPage/CourseOfferings.js
@@ -1,33 +1,76 @@
 import React, { Component } from 'react'
 import { findDOMNode } from 'react-dom'
-import PropTypes from 'prop-types'
 import HotTable from 'react-handsontable'
+import XLSX from 'xlsx'
 
 class CourseOfferings extends Component {
+  state = {
+    xlsx: [[]],
+    isValid: false,
+    showError: false
+  }
+
+  componentDidUpdate() {
+    this.validateCells()
+  }
+
   validateCells = () => {
     this.table.hotInstance.validateCells()
   }
 
-  componentDidUpdate () {
-    this.validateCells()
+  handleSubmit = () => {
+    const filtered = this.state.xlsx
+      .filter((row, index) => !this.table.hotInstance.isEmptyRow(index))
+    this.table.hotInstance.validateCells(isValid => {
+      this.setState({ showError: !isValid })
+    })
   }
 
-  handleSubmit = () => {
-    this.setState(prevState => ({
-      handsontableData: [
-        ...prevState.handsontableData
-          .filter((row, index) => !this.table.hotInstance.isEmptyRow(index))
-      ]
-    }), this.validateCells)
+  handleFileUpload = e => {
+    const files = e.target.files
+    if (!files || files.length == 0) return
+    const file = files[0]
+    const fileReader = new FileReader()
+    fileReader.onload = e => {
+      const filename = file.name
+      // pre-process data
+      let binary = ''
+      const bytes = new Uint8Array(e.target.result)
+      for (let i = 0; i < bytes.byteLength; i++) {
+        binary += String.fromCharCode(bytes[i])
+      }
+
+      const workbook = XLSX.read(binary, {type: 'binary'})
+      const sheet = workbook.Sheets[workbook.SheetNames[0]]
+      const xlsx = XLSX.utils.sheet_to_json(sheet, {header: 1, blankrows: false})
+      this.setState({
+        showError: false,
+        xlsx
+      }, this.validateCells)
+    }
+    fileReader.readAsArrayBuffer(file)
   }
 
   render() {
     return (
       <div>
+        <div className='admin-page-header'>
+          <h2>Adicionar Oferta</h2>
+          <input
+            type='file'
+            id='upload-planilha'
+            name='upload-planilha'
+            className='upload-input'
+            accept='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel'
+            onChange={this.handleFileUpload} />
+          <label
+            htmlFor='upload-planilha'
+            className='admin-page-link'>Upload Oferta</label>
+        </div>
         <HotTable 
           root='hot'
-          ref={(table) => { this.table = table; }}
-          data={this.props.xlsx} 
+          ref={(table) => { this.table = table }}
+          data={this.state.xlsx} 
           colHeaders={['Código de Oferta', 'Nome da Disciplina', 'Dia', 'Horário']}
           rowHeaders={true}
           minCols='4' 
@@ -46,13 +89,7 @@ class CourseOfferings extends Component {
             },
             {
               type: 'dropdown',
-              source: [
-                '8',
-                '10',
-                '12',
-                '14',
-                '16'
-              ]
+              source: ['8', '10', '12', '14', '16']
             }
           ]}
           beforeValidate={ (val, row, prop) => {
@@ -60,14 +97,14 @@ class CourseOfferings extends Component {
               return 0
             }
           }} />
-          <button onClick={this.handleSubmit}>submit</button>
+          { this.state.showError && 
+            <div className='admin-page-error'>Alguns campos da planilha possuem erros.</div> }
+          <button
+            className='admin-page-button'
+            onClick={this.handleSubmit}>Salvar</button>
       </div>
     )
   }
-}
-
-CourseOfferings.propTypes = {
-  xlsx: PropTypes.array
 }
 
 export default CourseOfferings
