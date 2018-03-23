b389400d05c9da8c69e54df163c374eeae5886b9
Juliana Bacelar <juliana.univasf@gmail.com>
2018-03-22 23:49:35 -0300

    Create home page

--- src/AdminPage/AdminPage.js
+++ src/AdminPage/AdminPage.js
@@ -3,13 +3,14 @@ import { Route, Switch } from 'react-router-dom'
 import './AdminPage.css'
 import CourseOfferings from './CourseOfferings'
 import AddCourseOfferings from './AddCourseOfferings'
+import HomePage from './HomePage'
 
 const Admin = ({match}) => {
   return (
     <div className='admin-page'>
       <Switch>
         <Route path={match.url + '/ofertas'} component={AddCourseOfferings}/>
-        <Route exact path={match.url} component={CourseOfferings}/>
+        <Route exact path={match.url} component={HomePage}/>
       </Switch>
     </div>
   )
---
+++ src/AdminPage/HomePage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import classIcon from '../static/class.png'
+import scheduleIcon from '../static/schedule.png'
+
+const HomePage = ({match}) => {
+  return (
+    <div className='home-page'>
+      <Link className='home-page-card' to='/admin/ofertas'>
+          <img className='home-page-card-image' src={classIcon} alt=""/>
+          Ofertas
+      </Link>
+      <Link to='/admin/ofertas' className='home-page-card'>
+        <img className='home-page-card-image' src={scheduleIcon} alt=""/>
+        Grade Curricular
+      </Link>
+    </div>
+  )
+}
+
+export default HomePage
