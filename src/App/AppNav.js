8dc099ab09b095bff7c542a5c7e20650360979d0
Juliana Bacelar <juliana.univasf@gmail.com>
2018-09-28 09:18:54 -0300

    Fix logout action

--- src/App/AppNav.js
+++ src/App/AppNav.js
@@ -1,29 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Menu from './Menu'
 import { Link } from 'react-router-dom'
 import { isLoggedIn } from '../utils/auth'
 import { clearInfo, ACCESS_AUTH_INFO } from '../utils/localStorage'
+import { Redirect } from 'react-router-dom'
 
 class AppHeader extends Component {
-  clearAuthInfo = () => {
+  state = {
+    toHome: false
+  } 
+
+  clearAuthInfo = e => {
+    e.preventDefault();
     clearInfo(ACCESS_AUTH_INFO)
+    this.setState({
+      toHome: true
+    })
   }
 
   render() {
     return (
-      <section className='app-nav'>
-        <div className='app-nav-container'>
-          { isLoggedIn(ACCESS_AUTH_INFO)
-            ? <a href='' className='login-button' onClick={this.clearAuthInfo}>
-                Logout
-              </a>
-            : <Link to='/login' className='login-button'>
-                Login
-              </Link>
-          }
-          { isLoggedIn(ACCESS_AUTH_INFO) && <Menu />}
-        </div>
-      </section>
+      <Fragment>
+        <section className='app-nav'>
+          <div className='app-nav-container'>
+            { isLoggedIn(ACCESS_AUTH_INFO)
+              ? <a className='login-button' onClick={this.clearAuthInfo}>
+                  Logout
+                </a>
+              : <Link to='/login' className='login-button'>
+                  Login
+                </Link>
+            }
+            { isLoggedIn(ACCESS_AUTH_INFO) && <Menu />}
+          </div>
+        </section>
+        { this.state.toHome && <Redirect to={'/'} />}
+      </Fragment>
     );
   }
 }
